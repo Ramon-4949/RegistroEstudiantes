6a20d507d7bd6bec4f1aa454949b2c23f86f8ab1
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2026-01-22 21:55:20 -0400

    Creando entity , dao y actualizacion de RoomDatabase

--- app/src/main/java/edu/ucne/registroestudiantes/Data/BD/EstudiantesDB.kt
+++ app/src/main/java/edu/ucne/registroestudiantes/Data/BD/EstudiantesDB.kt
@@ -2,15 +2,20 @@ package edu.ucne.registroestudiantes.Data.BD
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import edu.ucne.registroestudiantes.Data.Local.EstudianteEntity
 import edu.ucne.registroestudiantes.Data.Local.EstudianteDAO
+import edu.ucne.registroestudiantes.Data.Local.EstudianteEntity
+import edu.ucne.registroestudiantes.Data.Local.entities.AsignaturaEntity
+import edu.ucne.registroestudiantes.Data.Local.dao.AsignaturaDao
 
 @Database(
-    entities = [EstudianteEntity::class], // Aquí definimos la tabla de Estudiantes
-    version = 1,
+    entities = [
+        EstudianteEntity::class,
+        AsignaturaEntity::class
+    ],
+    version = 2,
     exportSchema = false
 )
 abstract class EstudiantesDB : RoomDatabase() {
-
     abstract fun estudianteDao(): EstudianteDAO
+    abstract fun asignaturaDao(): AsignaturaDao
 }
