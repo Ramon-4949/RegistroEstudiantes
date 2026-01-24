55ec3e640c4192bb03a3648e39c57d4583484b5c
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2026-01-24 15:51:56 -0400

    Implementando conversion .todomain() y to.Entity() al componente

--- app/src/main/java/edu/ucne/registroestudiantes/Domain/Repository/AsignaturaRepository.kt
+++ app/src/main/java/edu/ucne/registroestudiantes/Domain/Repository/AsignaturaRepository.kt
@@ -1,12 +1,12 @@
 package edu.ucne.registroestudiantes.Domain.Repository
 
-import edu.ucne.registroestudiantes.Data.Local.entities.AsignaturaEntity
+import edu.ucne.registroestudiantes.Domain.Model.Asignatura
 import kotlinx.coroutines.flow.Flow
 
 interface AsignaturaRepository {
-    suspend fun save(asignatura: AsignaturaEntity)
-    suspend fun delete(asignatura: AsignaturaEntity)
-    suspend fun find(id: Int): AsignaturaEntity?
-    suspend fun findByNombre(nombre: String): AsignaturaEntity?
-    fun getAll(): Flow<List<AsignaturaEntity>>
+    suspend fun save(asignatura: Asignatura)
+    suspend fun delete(asignatura: Asignatura)
+    suspend fun find(id: Int): Asignatura?
+    suspend fun findByNombre(nombre: String): Asignatura?
+    fun getAll(): Flow<List<Asignatura>>
 }
--- app/src/main/java/edu/ucne/registroestudiantes/Domain/Repository/AsignaturaRepositoryImpl.kt
+++ app/src/main/java/edu/ucne/registroestudiantes/Domain/Repository/AsignaturaRepositoryImpl.kt
@@ -1,17 +1,37 @@
 package edu.ucne.registroestudiantes.Data.Repository
 
 import edu.ucne.registroestudiantes.Data.Local.dao.AsignaturaDao
-import edu.ucne.registroestudiantes.Data.Local.entities.AsignaturaEntity
+import edu.ucne.registroestudiantes.Data.Mapper.toDomain
+import edu.ucne.registroestudiantes.Data.Mapper.toEntity
+import edu.ucne.registroestudiantes.Domain.Model.Asignatura
 import edu.ucne.registroestudiantes.Domain.Repository.AsignaturaRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class AsignaturaRepositoryImpl @Inject constructor(
     private val dao: AsignaturaDao
 ) : AsignaturaRepository {
-    override suspend fun save(asignatura: AsignaturaEntity) = dao.save(asignatura)
-    override suspend fun delete(asignatura: AsignaturaEntity) = dao.delete(asignatura)
-    override suspend fun find(id: Int): AsignaturaEntity? = dao.find(id)
-    override suspend fun findByNombre(nombre: String): AsignaturaEntity? = dao.findByNombre(nombre)
-    override fun getAll(): Flow<List<AsignaturaEntity>> = dao.getAll()
+
+    override suspend fun save(asignatura: Asignatura) {
+        dao.save(asignatura.toEntity())
+    }
+
+    override suspend fun delete(asignatura: Asignatura) {
+        dao.delete(asignatura.toEntity())
+    }
+
+    override suspend fun find(id: Int): Asignatura? {
+        return dao.find(id)?.toDomain()
+    }
+
+    override suspend fun findByNombre(nombre: String): Asignatura? {
+        return dao.findByNombre(nombre)?.toDomain()
+    }
+
+    override fun getAll(): Flow<List<Asignatura>> {
+        return dao.getAll().map { listaEntities ->
+            listaEntities.map { it.toDomain() }
+        }
+    }
 }
